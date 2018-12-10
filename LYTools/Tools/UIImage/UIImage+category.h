0f8aa394550df75317d8e8c821a3a10f60feda77
robertleel <994547393@qq.com>
2018-12-10 23:10:20 +0800

    modify

--- LYTools/Tools/UIImage/UIImage+category.h
+++ LYTools/Tools/UIImage/UIImage+category.h
@@ -10,6 +10,14 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef NS_ENUM(NSInteger, TLImageWaterLocation) {
+    TLImageWaterLocationTopLeft,
+    TLImageWaterLocationTopRight,
+    TLImageWaterLocationBottomLeft,
+    TLImageWaterLocationBottomRight,
+    TLImageWaterLocationCenter
+};
+
 @interface UIImage (category)
 
 /// 根据颜色生成一张图片
@@ -23,6 +31,37 @@ NS_ASSUME_NONNULL_BEGIN
 ///将图片裁剪到目标尺寸
 - (UIImage *)tl_imageScalingToTargetSize:(CGSize)targetSize;
 
+
+/**
+ 图片旋转角度
+
+ @param degrees 角度
+ @return 旋转完的角度
+ */
+- (UIImage *)tl_rotateImageByDegrees:(CGFloat)degrees;
+
+
+/**
+ 图片添加文字水印  默认距离边距为10
+
+ @param text 文字
+ @param fontSize 文字大小
+ @param color 文字颜色
+ @param location 水印位置
+ @return 加完水印的图
+ */
+- (UIImage *)tl_addWaterWithText:(NSString *)text fontSize:(CGFloat)fontSize textColor:(UIColor *)color location:(TLImageWaterLocation)location;
+
+
+/**
+ 图片添加图片水印  默认距离边距为10
+
+ @param image 水印图片
+ @param waterSize 水印大小
+ @param location 位置
+ @return 加完水印后的图片
+ */
+- (UIImage *)tl_addWaterWithImage:(UIImage *)image waterImageSize:(CGSize)waterSize location:(TLImageWaterLocation)location;
 
 @end
 
--- LYTools/Tools/UIImage/UIImage+category.m
+++ LYTools/Tools/UIImage/UIImage+category.m
@@ -8,6 +8,9 @@
 
 #import "UIImage+category.h"
 
+#define kImageWaterMarginX 10
+#define kImageWaterMarginY 10
+
 @implementation UIImage (category)
 
 + (UIImage *)imageWithColor:(UIColor *)color {
@@ -85,6 +88,124 @@
     }
     UIGraphicsEndImageContext();
     return newImage;
+}
+
+- (UIImage *)tl_rotateImageByDegrees:(CGFloat)degrees; {
+    CGFloat width = CGImageGetWidth(self.CGImage);
+    CGFloat height = CGImageGetHeight(self.CGImage);
+    
+    CGSize rotatedSize;
+    
+    rotatedSize.width = width;
+    rotatedSize.height = height;
+    
+    UIGraphicsBeginImageContext(rotatedSize);
+    CGContextRef bitmap = UIGraphicsGetCurrentContext();
+    CGContextTranslateCTM(bitmap, rotatedSize.width/2, rotatedSize.height/2);
+    CGContextRotateCTM(bitmap, degrees * M_PI / 180);
+    CGContextRotateCTM(bitmap, M_PI);
+    CGContextScaleCTM(bitmap, -1.0, 1.0);
+    CGContextDrawImage(bitmap, CGRectMake(-rotatedSize.width/2, -rotatedSize.height/2, rotatedSize.width, rotatedSize.height), self.CGImage);
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+- (UIImage *)tl_addWaterWithText:(NSString *)text fontSize:(CGFloat)fontSize textColor:(UIColor *)color location:(TLImageWaterLocation)location {
+    CGSize size = self.size;
+    
+    CGRect rect = (CGRect){CGPointZero,size};
+    
+    //新建图片图形上下文
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0f);
+    
+    //绘制图片
+    [self drawInRect:rect];
+    
+    //绘制文本
+    NSDictionary *attr =@{NSFontAttributeName : [UIFont systemFontOfSize:fontSize],NSForegroundColorAttributeName:color};
+    
+    CGRect strRect = [self tl_calculateWidth:text attr:attr location:location rect:rect];
+    
+    [text drawInRect:strRect withAttributes:attr];
+    
+    //获取图片
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    //结束图片图形上下文
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+
+- (UIImage *)tl_addWaterWithImage:(UIImage *)image waterImageSize:(CGSize)waterSize location:(TLImageWaterLocation)location {
+    CGSize size = self.size;
+    
+    CGRect rect = (CGRect){CGPointZero,size};
+    
+    //新建图片图形上下文
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0f);
+    
+    //绘制图片
+    [self drawInRect:rect];
+    
+    //计算水印的rect
+    CGSize waterImageSize = CGSizeEqualToSize(waterSize, CGSizeZero)?image.size:waterSize;
+    CGRect calRect = [self tl_rectWithRect:rect size:waterImageSize location:location];
+    
+    //绘制水印图片
+    [image drawInRect:calRect];
+    
+    //获取图片
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    //结束图片图形上下文
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+#pragma mark- private Method
+/**
+ 计算水印位置
+
+ @param text 水印文字
+ @param attr 字符串属性
+ @param location 位置
+ @param rect 图片rect
+ @return rect
+ */
+- (CGRect)tl_calculateWidth:(NSString *)text attr:(NSDictionary *)attr location:(TLImageWaterLocation)location rect:(CGRect)rect {
+     CGSize size =  [text sizeWithAttributes:attr];
+    CGRect calRect = [self tl_rectWithRect:rect size:size location:location];
+    return calRect;
+}
+
+- (CGRect)tl_rectWithRect:(CGRect)rect size:(CGSize)size location:(TLImageWaterLocation)location {
+    CGPoint point = CGPointZero;
+    //右上
+    if(TLImageWaterLocationTopRight == location){
+        point = CGPointMake(rect.size.width - size.width - kImageWaterMarginX, kImageWaterMarginY);
+    }
+    //左下
+    if(TLImageWaterLocationBottomLeft == location) {
+        point = CGPointMake(kImageWaterMarginX, rect.size.height - size.height - kImageWaterMarginY);
+    }
+    //右下
+    if(TLImageWaterLocationBottomRight == location) {
+        point = CGPointMake(rect.size.width - size.width - kImageWaterMarginX, rect.size.height - size.height - kImageWaterMarginY);
+    }
+    //左上
+    if (TLImageWaterLocationTopLeft == location) {
+    }
+    //正中
+    if(TLImageWaterLocationCenter == location){
+        point = CGPointMake((rect.size.width - size.width)*.5f, (rect.size.height - size.height)*.5f);
+    }
+    CGRect calRect = (CGRect){point,size};
+    
+    return calRect;
 }
 
 @end
